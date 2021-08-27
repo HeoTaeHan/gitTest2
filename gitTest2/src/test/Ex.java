df97f0535073657467c611a59712fdc069164654
user <user@DESKTOP-HM1GJ50>
2021-08-27 13:49:58 +0900

    Commit 별 지우고 test 추가

--- gitTest2/src/test/Ex.java
+++ gitTest2/src/test/Ex.java
@@ -2,10 +2,6 @@ package test;
 
 public class Ex {
 	public static void main(String[] args) {
-		System.out.println("*");
-		System.out.println("**");
-		System.out.println("***");
-		System.out.println("****");
-		System.out.println("*****");
+		System.out.println("test");
 	}
 }
